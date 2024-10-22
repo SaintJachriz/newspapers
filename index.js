a014aadaf47c492d8d0d0ac45508e22bbf8e4f62
SaintJachriz <mbagwujachi03@gmail.com>
2024-10-22 18:27:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function Show(){
+    const hide = document.getElementById('hidden');
+    hide.style.display = 'block';  // show the hidden div
+}
+
+
+function Hide(){
+    const hide = document.getElementById('hidden');
+    hide.style.display = 'none';
+}
